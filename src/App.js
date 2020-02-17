553faa2d8d636b0a56efd2bdb579eabfd27881d7
wangbin <316281400@qq.com>
2020-02-17 14:49:57 +0800

    初始化项目

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react'
+import Header from './components/Header'
 
 function App() {
   return (
-    <div className="dell">
-      hello world
-    </div>
+    <Header/>
   )
 }
 
---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header-wrapper">
+        <a href="/" className="logo"> </a>
+        <div className="nav">
+
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Header
